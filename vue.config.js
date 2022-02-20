64843b189e6e0ca58e08d67fa05203e63245307d
Johan <johan@romantech.net>
2022-02-21 02:16:40 +0900

    Fix vue.config.js comment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+// reference : https://cli.vuejs.org/zh/guide/deployment.html
+// publicPath 에 아무것도 입력하지 않으면 https://<USERNAME>.github.io/ 주소로 접속됨
+// https://<USERNAME>.github.io/<REPO>/ 주소로 접속하고 싶으면
+// publicPath 속성에 프로젝트 이름 입력 ex) /project-name/
+
 module.exports = {
-  publicPath: '/practice-vue-i18n/',
-  outputDir: 'docs',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/practice-vue-i18n/' : '/',
+  outputDir: 'docs', // 빌드 output 폴더
 };
