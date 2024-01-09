d8d409bb0c116f7311beab921ade161154cd2c0f
ColorFilter <johan@romantech.net>
2024-01-10 01:54:23 +0900

    레거시 오류 해결

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
-// reference : https://cli.vuejs.org/zh/guide/deployment.html
-// publicPath 에 아무것도 입력하지 않으면 https://<USERNAME>.github.io/ 주소로 접속됨
-// https://<USERNAME>.github.io/<REPO>/ 주소로 접속하고 싶으면
-// publicPath 속성에 프로젝트 이름 입력 ex) /project-name/
-
+// vue.config.js
 module.exports = {
+  chainWebpack: config => {
+    config.plugin('define').tap(definitions => {
+      Object.assign(definitions[0], {
+        // Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined... 오류 해결
+        // https://vuejs.org/api/compile-time-flags
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
+      });
+      return definitions;
+    });
+  },
+  // reference : https://cli.vuejs.org/zh/guide/deployment.html
+  // publicPath 에 아무것도 입력하지 않으면 https://<USERNAME>.github.io/ 주소로 접속됨
+  // https://<USERNAME>.github.io/<REPO>/ 주소로 접속하고 싶으면
+  // publicPath 속성에 프로젝트 이름 입력 ex) /project-name/
   publicPath:
     process.env.NODE_ENV === 'production' ? '/practice-vue-i18n/' : '/',
   outputDir: 'docs', // 빌드 output 폴더
